14f2cb4dee02bd85b6a5d3e8c12ac67a639a7ace
Atsushi Kobayashi <nekokak@gmail.com>
2010-01-26 00:38:35 +0900

    だいたいつくった（コピペした

--- lib/Test/Fixture/DBIxSkinny.pm
+++ lib/Test/Fixture/DBIxSkinny.pm
@@ -2,6 +2,78 @@ package Test::Fixture::DBIxSkinny;
 use strict;
 use warnings;
 our $VERSION = '0.01';
+use base 'Exporter';
+our @EXPORT = qw/construct_fixture/;
+use Params::Validate ':all';
+use Carp ();
+use Kwalify ();
+
+sub construct_fixture {
+    my %args = validate(
+        @_ => +{
+            db      => 1,
+            fixture => 1,
+        }
+    );
+
+    my $fixture = _validate_fixture(_load_fixture($args{fixture}));
+    _delete_all($args{db});
+    return _insert($args{db}, $fixture);
+}
+
+sub _load_fixture {
+    my $stuff = shift;
+
+    if (ref $stuff) {
+        if (ref $stuff eq 'ARRAY') {
+            return $stuff;
+        } else {
+            Carp::croak "invalid fixture stuff. should be ARRAY: $stuff";
+        }
+    } else {
+        require YAML::Syck;
+        return YAML::Syck::LoadFile($stuff);
+    }
+}
+
+sub _validate_fixture {
+    my $stuff = shift;
+
+    Kwalify::validate(
+        {
+            type     => 'seq',
+            sequence => [
+                {
+                    type    => 'map',
+                    mapping => {
+                        table => { type => 'str', required => 1 },
+                        name  => { type => 'str', required => 1 },
+                        data  => { type => 'any', required => 1 },
+                    },
+                }
+            ]
+        },
+        $stuff
+    );
+
+    $stuff;
+}
+
+sub _delete_all {
+    my $db = shift;
+    $db->delete($_) for
+        keys %{$db->schema->schema_info};
+}
+
+sub _insert {
+    my ($db, $fixture) = @_;
+
+    my $result = {};
+    for my $row ( @{ $fixture } ) {
+        $result->{ $row->{name} } = $db->insert($row->{table}, $row->{data});
+    }
+    return $result;
+}
 
 1;
 __END__
---
+++ t/lib/Mock/DB.pm
@@ -0,0 +1,16 @@
+package Mock::DB;
+use DBIx::Skinny;
+
+package Mock::DB::Schema;
+use DBIx::Skinny::Schema;
+
+install_table foo => schema {
+    pk 'id';
+    columns qw/
+        id
+        name
+    /;
+};
+
+1;
+
