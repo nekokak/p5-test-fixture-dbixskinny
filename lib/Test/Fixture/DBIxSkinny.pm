45347c10402bb58c5ddccad5cb91609313ba5c56
Atsushi Kobayashi <nekokak@gmail.com>
2010-01-25 23:15:43 +0900

    initial commit

---
+++ lib/Test/Fixture/DBIxSkinny.pm
@@ -0,0 +1,32 @@
+package Test::Fixture::DBIxSkinny;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Test::Fixture::DBIxSkinny -
+
+=head1 SYNOPSIS
+
+  use Test::Fixture::DBIxSkinny;
+
+=head1 DESCRIPTION
+
+Test::Fixture::DBIxSkinny is
+
+=head1 AUTHOR
+
+Atsushi Kobayashi E<lt>nekokak _at_ gmail _dot_ comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
